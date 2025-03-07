60df2dff065b36d819bf0cfd293b8c393e2ab4cc
Ellen Whalen <161654619+ellenwhalen@users.noreply.github.com>
2025-03-07 23:11:38 +0000

    Referenced style.css

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
    rel="stylesheet" crossorigin="anonymous"
    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">
+        <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
         <!-- Responsive navbar-->
@@ -26,7 +27,14 @@
         <!-- Page content-->
         <div class="container mt-5">
 
-        
+        <form method="get" action="carousel.php"> <!-- action - when the user clicks, where to go?-->
+            <select name="myselect">
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+            <input type="submit" value="click here">
+        </form>
 
 
 
