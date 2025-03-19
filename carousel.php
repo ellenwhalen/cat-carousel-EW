d93802d2859c0043ef8f42470e611d894afa0736
Ellen Whalen <161654619+ellenwhalen@users.noreply.github.com>
2025-03-19 05:10:00 +0000

    carousel technically functioning

--- carousel.php
+++ carousel.php
@@ -25,14 +25,43 @@
                 <a class="navbar-brand" href="index.php">Cat Carousel</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        
+                
                 </div>
             </div>
         </nav>
         <!-- Page content-->
         <div class="container mt-5">
-
-        
+            <?php
+                getImages();
+                $imgUrlArray = $_SESSION["imgUrlArray"];
+            ?>
+            <!-- <img src='https://cdn2.thecatapi.com/images/xnzzM6MBI.jpg' class="d-block w-100" alt="Cat 1"> -->
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9" aria-label="Slide 10"></button>
+                </div>
+                <div class="carousel-inner">
+                    <?php fillCarousel(); ?>
+                    
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+                </div>
 
 
 
--- src/functions.php
+++ src/functions.php
@@ -7,27 +7,21 @@
 /* write PHP functions here */
 
     function initializePage() {
-
-        // $url = "http://api.thecatapi.com/v1/breeds?api_key=" . API_KEY;
         $url = "http://api.thecatapi.com/v1/breeds";
         $breedInfoRaw = file_get_contents($url);
         $breedInfo = json_decode($breedInfoRaw);
         $_SESSION["breedInfo"] = $breedInfo;
-        // echo($breedInfo[0]->name);
-        // echo(count($breedInfo));
-        // echo($_SESSION["breedInfo"]);
-        // for ($i=0; $i++; $i < count($breedInfo)) {
-        // }
     }
 
     function initializeSelectForm() {
         $formHtml = '<div class="row">
             <div class="col-sm-7"><form method="get" action="carousel.php">
-            <select name="catID" class="form-select">';
+            <select name="catId" class="form-select">';
         $breedInfo = $_SESSION["breedInfo"];
         for ($i=0; $i < count($breedInfo); $i++) {
             $thisName = $breedInfo[$i]->name;
-            $formHtml .= '<option value="' . $thisName . '">' . $thisName . '</option>';
+            $thisId = $breedInfo[$i]->id;
+            $formHtml .= '<option value="' . $thisId . '">' . $thisName . '</option>';
         }
         $formHtml .= '</select>
             </div>
@@ -39,5 +33,29 @@
         echo($formHtml);
     }
 
+    function getImages() {
+        $url = "https://api.thecatapi.com/v1/images/search?breed_ids=" . $_GET["catId"] . "&limit=10";
+        $imagesRaw = file_get_contents($url);
+        $images = json_decode($imagesRaw);
+        $imgUrlArray = [];
+        for ($i=0; $i < 10; $i++) {
+            $imgUrl = $images[$i]->url;
+            $imgUrlArray[$i] = $imgUrl;
+        }
+        $_SESSION["imgUrlArray"] = $imgUrlArray;
+    }
+
+    function fillCarousel() {
+        $imgUrlArray = $_SESSION["imgUrlArray"];
+        $imageHtml = '';
+        for ($i=0; $i < count($imgUrlArray); $i++) {
+            $imageHtml .= '<div class="carousel-item';
+            if ($i == 0) {
+                $imageHtml .= ' active';
+            }
+            $imageHtml .= '"> <img src=' . $imgUrlArray[$i] .  ' class="d-block w-100" alt="Cat ' . ($i + 1) . '"> </div>';
+        }
+        echo($imageHtml);
+    }
 ?>
 
